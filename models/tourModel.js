791db2deaf4caf411ed3cd9cd7fe0e5449df7b35
Jhonatan Nascimento <j.nascimento3138@gmail.com>
2024-01-11 01:19:48 -0300

    Finish Modeling Data and Advanced Mongoose

--- models/tourModel.js
+++ models/tourModel.js
@@ -46,6 +46,7 @@ const tourSchema = new mongoose.Schema(
             default: 4.5,
             min: [1, 'Rating must be above 1.0'],
             max: [5, 'Rating must be below 5.0'],
+            set: (val) => Math.round(val * 10) / 10,
         },
         ratingsQuantity: {
             type: Number,
@@ -131,6 +132,8 @@ const tourSchema = new mongoose.Schema(
 tourSchema.index({ price: 1, ratingsAverage: -1 });
 tourSchema.index({ slug: 1 });
 
+tourSchema.index({ startLocation: '2dsphere' });
+
 tourSchema.virtual('durationWeeks').get(function () {
     return this.duration / 7;
 });
@@ -190,11 +193,11 @@ tourSchema.post(/^find/, function (docs, next) {
 });
 
 //Aggregation middleware
-tourSchema.pre('aggregate', function (next) {
-    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-    console.log(this);
-    next();
-});
+// tourSchema.pre('aggregate', function (next) {
+//     this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//     console.log(this);
+//     next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
